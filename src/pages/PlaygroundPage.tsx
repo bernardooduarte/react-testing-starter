316adf04a4142641953b1279c70052980f9587f0
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-12-23 15:50:51 -0300

    fazendo o curso

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
+import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+  return <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium delectus perspiciatis voluptas, magnam fugit. Soluta, et architecto placeat quis amet consequuntur esse sapiente iure, numquam impedit necessitatibus deleniti, non rem fuga in! Ducimus iste labore deserunt voluptatem. Molestias perferendis illum labore maiores. Porro eligendi repudiandae alias sequi voluptas enim dolore sint reprehenderit consectetur possimus, ad iusto omnis assumenda repellendus voluptates laboriosam deserunt aperiam molestias, fugiat minima dolores? Cum iure odio possimus beatae a? Atque enim laboriosam cumque voluptatum beatae cum ducimus, fugit laborum velit sit assumenda deleniti asperiores vitae possimus dicta. Reiciendis dolorum quasi facere nobis temporibus minima ut." />;
 };
 
 export default PlaygroundPage;
